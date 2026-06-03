9fd180c8514715549afbd3c0159a9588db32cb1e
Danyka Byrnes <danyka.byrnes@gmail.com>
2026-06-03 18:29:00 -0400

    creating new figures for yield vs. watershed feature, with markers differentiated by colour (region) and size (drainage area)

--- config.py
+++ config.py
@@ -4,14 +4,14 @@ OUTPUT_filepath = "../OUTPUT/"
 metadata_filepath = INPUT_filepath+"01_metadata/metadata.csv"
 shapefile_filepath = INPUT_filepath+'03_shapefiles/'
 anc_data_filepath = INPUT_filepath+"00_additional_data/"
-load_filepath = INPUT_filepath+"02_water_quality_data/load_flex_filled/"
+load_filepath = INPUT_filepath+"02_water_quality_data/"
 land_use_filepath = INPUT_filepath+"07_dynamic_lulc/"
 
 # Data metadata
-start_year = 2008
+START_YEAR = 2008
 
 # SITE IDs
-main_sites=['STREAM-gauge-2891',
+MAIN_SITES = ['STREAM-gauge-2891',
             'STREAM-gauge-2886',
             'STREAM-gauge-2903',
             'STREAM-gauge-2962',
@@ -41,10 +41,40 @@ main_sites=['STREAM-gauge-2891',
             'STREAM-gauge-695'
 ]
 
-site_name_dict = []
+# Names for stations
+SHORTHAND_NAMES = {
+    'STREAM-gauge-2891': 'IL-EM',
+    'STREAM-gauge-2886': 'IL-SP',
+    'STREAM-gauge-2903': 'IL-DP',
+    'STREAM-gauge-2962': 'IL-KA',
+    'STREAM-gauge-2963': 'IL-BM',
+    'STREAM-gauge-3092': 'IA-IA',
+    'STREAM-gauge-3096': 'IA-DM',
+    'STREAM-gauge-3097': 'IA-NR',
+    'STREAM-gauge-3077': 'IA-TR',
+    'STREAM-gauge-3089': 'IA-CR',
+    'STREAM-gauge-3100': 'IA-SR',
+    'STREAM-gauge-3108': 'IA-NI',
+    'STREAM-gauge-3109': 'IA-NO',
+    'STREAM-gauge-308':  'LA-MS',
+    'STREAM-gauge-2203': 'MO-MO',
+    'STREAM-gauge-4472': 'IN-IR',
+    'STREAM-gauge-4431': 'IN-WW',
+    'STREAM-gauge-4440': 'IN-EC',
+    'STREAM-gauge-4442': 'IN-SB',
+    'STREAM-gauge-4465': 'IN-KA',
+    'STREAM-gauge-2804': 'KS-KS',
+    'STREAM-gauge-2816': 'KS-LA',
+    'STREAM-gauge-3776': 'KY-GR',
+    'STREAM-gauge-3809': 'OH-OH',
+    'STREAM-gauge-NA1':  'IN-SD',
+    'STREAM-gauge-NA2':  'IN-KD',
+    'STREAM-gauge-695':  'NE-ER',
+    'STREAM-gauge-692':  'NE-PR',
+}
 
 # Animal units. Workbook is found i Inputs > 00_additional_data > livestock_density > AU_animal_workbook.xlsx
-au = {1047:0.83, # Beef cow
+AU = {1047:0.83, # Beef cow
       2047:0.74, # Milk cow
       5047:1.26, # Other cattle
       1051:8.57, # Hogs + pigs
